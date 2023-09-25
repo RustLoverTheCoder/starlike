aee230d418e56bfc17221d1b4944821c12329299
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-09-25 22:58:53 +0800

    feat: add swiper

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect } from "react";
 import { GameIntroduction } from "./GameIntroduction";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Mousewheel, HashNavigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const HomePage = () => {
   return (
-    <div>
-      <GameIntroduction />
-    </div>
+    <Swiper
+      direction={"vertical"}
+      slidesPerView={1}
+      spaceBetween={30}
+      mousewheel={true}
+      hashNavigation={{
+        watchState: true,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Mousewheel, Pagination, HashNavigation]}
+      className="mySwiper"
+    >
+      <SwiperSlide data-hash="slide1">Slide 1</SwiperSlide>
+      <SwiperSlide data-hash="slide2">Slide 2</SwiperSlide>
+      <SwiperSlide data-hash="slide3">Slide 3</SwiperSlide>
+      <SwiperSlide data-hash="slide4">Slide 4</SwiperSlide>
+      <SwiperSlide data-hash="slide5">Slide 5</SwiperSlide>
+      <SwiperSlide data-hash="slide6">Slide 6</SwiperSlide>
+      <SwiperSlide data-hash="slide7">Slide 7</SwiperSlide>
+    </Swiper>
   );
 };
