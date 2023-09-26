0b09f723efcb447bc93169032d280930ac41efca
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-09-26 23:20:26 +0800

    feat: add header

---
+++ src/components/events/index.tsx
@@ -0,0 +1,7 @@
+export const Events = () => {
+  return (
+    <div className="w-screen h-screen relative" style={{}}>
+      <img src={`/images/events/18.jpg`} alt="events" className="w-full h-full object-cover object-center" />
+    </div>
+  );
+};
