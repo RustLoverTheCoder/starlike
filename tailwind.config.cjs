b0f0aeea4cc346693a8de4ddd5930a48d2473562
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-09-26 18:28:19 +0800

    feat: add images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      zIndex: {
+        default: 1,
+        header: 10,
+        popup: 890, // 系统提示弹出层
+        system: 990,
+        top: 9999, // 最顶层
+      },
+    },
+  },
+  plugins: [],
+};
