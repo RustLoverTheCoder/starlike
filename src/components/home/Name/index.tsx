69fb6be79f776f69dea3780607ccb7ef88681748
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-09-27 19:06:42 +0800

    feat: add name

---
+++ src/components/home/Name/index.tsx
@@ -0,0 +1,48 @@
+export const Name = () => {
+  const wordPics = [
+    "/images/home/word/word_s.png",
+    "/images/home/word/word_t.png",
+    "/images/home/word/word_a.png",
+    "/images/home/word/word_r.png",
+    "/images/home/word/word_l.png",
+    "/images/home/word/word_i.png",
+    "/images/home/word/word_k.png",
+    "/images/home/word/word_e.png",
+  ];
+  const wordPicsActive = [
+    "/images/home/word/active/word_s.png",
+    "/images/home/word/active/word_t.png",
+    "/images/home/word/active/word_a.png",
+    "/images/home/word/active/word_r.png",
+    "/images/home/word/active/word_l.png",
+    "/images/home/word/active/word_i.png",
+    "/images/home/word/active/word_k.png",
+    "/images/home/word/active/word_e.png",
+  ];
+  return (
+    <div className="absolute ml-[-19%] w-[38vw] h-6 text-center left-2/4 top-[33%]">
+      <div className="hy-name-content hy-name-active relative flex items-center">
+        {wordPics.map((i) => {
+          return (
+            <img
+              src={i}
+              alt=""
+              className="inline-block h-6 opacity-100 mr-[10%]"
+            />
+          );
+        })}
+      </div>
+      <div className="hy-name-content hy-name-source relative flex items-center">
+        {wordPicsActive.map((i) => {
+          return (
+            <img
+              src={i}
+              alt=""
+              className="inline-block h-6 opacity-0 mr-[10%]"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
