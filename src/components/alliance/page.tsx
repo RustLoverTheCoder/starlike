6ff7db72cd336991f3bf1bf19c6fdbb65d4cd5e2
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-10-14 12:58:44 +0800

    feat: add info

--- src/components/alliance/page.tsx
+++ src/components/alliance/page.tsx
@@ -2,7 +2,13 @@ export interface AlliancePageType {
   type: "selene" | "moutainpass" | "federation" | "empire" | "cpgu";
 }
 
-export const AlliancePage = ({ type }: { type: string }) => {
+type Info = {
+  title: string;
+  subTitle: string;
+  data: { key: string; value: string }[];
+};
+
+export const AlliancePage = ({ type, info }: { type: string; info: Info }) => {
   return (
     <div className="w-full h-full relative max-w-full max-h-full overflow-hidden">
       <img
@@ -25,6 +31,23 @@ export const AlliancePage = ({ type }: { type: string }) => {
           alt=""
           className="h-full"
         />
+      </div>
+      {/* info */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+        <div className="container flex flex-col">
+          <div className="text-7xl bg-[linear-gradient(#fffefe,#9e9aa1)] bg-clip-text mb-2">
+            {info.title}
+          </div>
+          <div className="mb-10 text-2xl">{info.subTitle}</div>
+          <div className="flex flex-col">
+            {info.data.map((item) => (
+              <div className="flex items-centers text-sm leading-7">
+                <div>【{item.key}】：</div>
+                <div>{item.value}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
