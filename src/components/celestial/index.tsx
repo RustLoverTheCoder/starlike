cdef853fe9d558dece32fe2e8c0fb9ac901ceca8
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-10-14 17:16:47 +0800

    feat: add ordinaryStar

--- src/components/celestial/index.tsx
+++ src/components/celestial/index.tsx
@@ -1,12 +1,39 @@
 import { cn } from "@utils/cn";
 import { useState } from "react";
+const FeaturedStars = [
+  {
+    id: 0,
+    name: "行星状星云",
+    subTitle: "BlackHole",
+    image: "/images/celestial/行星状星云.webp",
+    info: "自从我们被迫离开了地球故乡，开始了千年的银河流浪，人类孤独地来到了无垠的宇宙之中。生存下来，是我们目前最关注的事情作为空间站的指挥官、我们必须面对多样复杂的各种突发事件、谨慎选择我们的策略来平衡民众、各大势力、血统、思潮、以及星语者的各种需求。同时我们必须守护着人类的火种，以及这文明数千年的约定",
+    infoImage: "",
+  },
+];
+
+const OrdinaryStars = [
+  {
+    id: 0,
+    name: "火山型星云",
+    subTitle: "VOLCANIC PLANET",
+    image: "/images/celestial/ordinaryStar0.webp",
+    info: "自从我们被迫离开了地球故乡，开始了千年的银河流浪，人类孤独地来到了无垠的宇宙之中。生存下来，是我们目前最关注的事情 作为空间站的指挥官、我们必须面对多样复杂的各种突发事件、谨慎选择我们的策略来平衡民众、各大势力、血统、思潮、以及星语者的各种需求。同时 我们必须守护着人类的火种，以及这文明数千年的约定",
+    infoImage: "",
+  },
+];
 
 export const CelestialPage = () => {
   const [activeNav, setActiveNav] = useState(0);
+  const [featuredStar, setFeaturedStar] = useState(0);
+  const [ordinaryStar, setOrdinaryStar] = useState(0);
   return (
     <div className="w-screen h-screen relative">
       <img
-        src="/images/celestial/行星状星云.webp"
+        src={
+          activeNav === 0
+            ? FeaturedStars[featuredStar].image
+            : OrdinaryStars[ordinaryStar].image
+        }
         alt=""
         className="w-full h-full object-cover object-center"
       />
@@ -14,8 +41,16 @@ export const CelestialPage = () => {
         <div className="container max-w-[690px]">
           <div className="title w-full h-auto text-right flex justify-end items-center">
             <div className="flex flex-col">
-              <div className="text-6xl mb-2">行星状星云</div>
-              <div className="text-2xl uppercase">BlackHole</div>
+              <div className="text-6xl mb-2">
+                {activeNav === 0
+                  ? FeaturedStars[featuredStar].name
+                  : OrdinaryStars[ordinaryStar].name}
+              </div>
+              <div className="text-2xl uppercase">
+                {activeNav === 0
+                  ? FeaturedStars[featuredStar].subTitle
+                  : OrdinaryStars[ordinaryStar].subTitle}
+              </div>
             </div>
             <div className="h-[100px] w-10 shadow-[0_0_10px_rgba(255,179,70,0.56),0_0_10px_rgba(255,179,70,0.56),0_0_10px_inset_rgba(255,179,70,0.56),0_0_10px_inset_rgba(255,179,70,0.56)] ml-6 border-2 border-solid border-[#ffd790]"></div>
           </div>
@@ -24,9 +59,32 @@ export const CelestialPage = () => {
             className="text-left tracking-[0px] text-white opacity-100"
             style={{ textShadow: "-5px 0px 9px #000000" }}
           >
-            自从我们被迫离开了地球故乡，开始了千年的银河流浪，人类孤独地来到了无垠的宇宙之中。生存下来，是我们目前最关注的事情
-            作为空间站的指挥官、我们必须面对多样复杂的各种突发事件、谨慎选择我们的策略来平衡民众、各大势力、血统、思潮、以及星语者的各种需求。同时
-            我们必须守护着人类的火种，以及这文明数千年的约定s
+            {activeNav === 0
+              ? FeaturedStars[featuredStar].info
+              : OrdinaryStars[ordinaryStar].info}
+          </div>
+          <div className="w-full">
+            {activeNav === 0 ? (
+              <>
+                {FeaturedStars[featuredStar].infoImage && (
+                  <img
+                    src={FeaturedStars[featuredStar].infoImage}
+                    alt=""
+                    className="w-[400px]"
+                  />
+                )}
+              </>
+            ) : (
+              <>
+                {OrdinaryStars[ordinaryStar].infoImage && (
+                  <img
+                    src={OrdinaryStars[ordinaryStar].infoImage}
+                    alt=""
+                    className="w-[400px]"
+                  />
+                )}
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -40,7 +98,10 @@ export const CelestialPage = () => {
       </div>
       {/* nav */}
       <div className="absolute top-[20%] left-[58px] flex flex-col z-10">
-        <div className="flex flex-col cursor-pointer" onClick={() => setActiveNav(0)}>
+        <div
+          className="flex flex-col cursor-pointer"
+          onClick={() => setActiveNav(0)}
+        >
           <div
             className={cn(
               "text-2xl transition-all",
